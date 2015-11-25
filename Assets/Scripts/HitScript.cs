a1c027ba9075a34bf26084f6f0806ec6745b1c80
Aldo Culquicondor <alculquicondor@gmail.com>
2015-11-25 12:44:48 -0500

    first sounds

---
+++ Assets/Scripts/HitScript.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+using Vuforia;
+
+public class HitScript : MonoBehaviour, IVirtualButtonEventHandler {
+
+    private GameObject glare;
+    private AudioSource sound;
+
+	void Start () {
+        sound = GetComponentInChildren<AudioSource>();
+        glare = transform.FindChild("Glare").gameObject;
+
+        GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+	}
+	
+	void Update () {
+	
+	}
+
+    public void OnButtonPressed(VirtualButtonAbstractBehaviour vb)
+    {
+        Debug.Log("Pressed");
+        sound.Play();
+        glare.SetActive(true);
+    }
+
+    public void OnButtonReleased(VirtualButtonAbstractBehaviour vb)
+    {
+        glare.SetActive(false);
+    }
+
+}
