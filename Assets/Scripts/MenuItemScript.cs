bdf3aeff1f5380f3d9cee205e6f0a2eb7f1c9357
Adolfo Tamayo <adolfo.tamayo@ucsp.edu.pe>
2015-12-02 13:39:30 -0500

    Pausing works

--- Assets/Scripts/MenuItemScript.cs
+++ Assets/Scripts/MenuItemScript.cs
@@ -12,25 +12,40 @@ public class MenuItemScript: MonoBehaviour {
 	public ButtonType buttonType;
 	public GameObject pauseMenu;
 
+	private MetronomeScript metronome;
 	private Transform originalParent;
 	private CardboardHead head;
 	private Vector3 startingPosition;
 	private float delay = 0.0f; 
+	private Transform buttonText;
+	private GameObject song;
+
 	
 	void Start() {
 		head = Camera.main.GetComponent<StereoController>().Head;
 		originalParent = pauseMenu.transform.parent;
+		buttonText = transform.GetChild (1);
+		buttonText.gameObject.SetActive (false);
+		song = GameObject.FindWithTag("Song");
+		metronome = FindObjectOfType<MetronomeScript>();
+
+		
 	}
 	
 	void Update() {
 		RaycastHit hit;
 		bool isLookedAt = GetComponent<Collider>().Raycast(head.Gaze, out hit, Mathf.Infinity);
+
 		if (isLookedAt) { 
+			buttonText.gameObject.SetActive(true);
 			if(Cardboard.SDK.Triggered){
 				switch(buttonType){
 					case ButtonType.play:
 						pauseMenu.SetActive(false);
 						pauseMenu.transform.SetParent(originalParent);
+						if(metronome.playing){
+							metronome.TogglePlay();
+						}
 						break;
 					case ButtonType.restart:
 						break;
@@ -46,7 +61,7 @@ public class MenuItemScript: MonoBehaviour {
 
 		} else if(!isLookedAt) { 
 			Debug.Log("Not Looked");
-
+			buttonText.gameObject.SetActive(false);
 			GetComponent<Renderer>().material = inactiveMaterial; 
 			transform.localScale.Set(3, 3, 1); 
 		}
--- Assets/Scripts/UIScript.cs
+++ Assets/Scripts/UIScript.cs
@@ -7,17 +7,25 @@ public class UIScript : MonoBehaviour {
 
 	private bool showingMenu;
 	private CardboardHead head;
+	private GameObject song;
+	private MetronomeScript metronome;
 
 	void Start () {
 		head = Camera.main.GetComponent<StereoController>().Head;
 		pauseMenu.SetActive (false);
 		showingMenu = false;
+		song = GameObject.FindWithTag("Song");
+		metronome = FindObjectOfType<MetronomeScript>();
+
 		Debug.Log ("Setting active false");
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Cardboard.SDK.Triggered) {
+				if(metronome.playing){
+					metronome.TogglePlay();
+				}
 				pauseMenu.transform.SetParent(null);
 				pauseMenu.SetActive(true);
 				showingMenu = true;
