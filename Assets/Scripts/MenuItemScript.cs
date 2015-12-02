8ecd58b0b5aa69c44849035416fe2da756367022
Adolfo Tamayo <adolfo.tamayo@ucsp.edu.pe>
2015-12-02 12:34:05 -0500

    Menu merged with main scene

--- Assets/Scripts/MenuItemScript.cs
+++ Assets/Scripts/MenuItemScript.cs
@@ -1,35 +1,53 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+public enum ButtonType{
+	play,restart,song,ambient
+}
 public class MenuItemScript: MonoBehaviour {
 
 
 	public Material inactiveMaterial;
 	public Material activeMaterial;
+	public ButtonType buttonType;
+	public GameObject pauseMenu;
 
+	private Transform originalParent;
 	private CardboardHead head;
 	private Vector3 startingPosition;
 	private float delay = 0.0f; 
 	
 	void Start() {
 		head = Camera.main.GetComponent<StereoController>().Head;
-		startingPosition = transform.localPosition;
+		originalParent = pauseMenu.transform.parent;
 	}
 	
 	void Update() {
 		RaycastHit hit;
 		bool isLookedAt = GetComponent<Collider>().Raycast(head.Gaze, out hit, Mathf.Infinity);
 		if (isLookedAt) { 
+			if(Cardboard.SDK.Triggered){
+				switch(buttonType){
+					case ButtonType.play:
+						pauseMenu.SetActive(false);
+						pauseMenu.transform.SetParent(originalParent);
+						break;
+					case ButtonType.restart:
+						break;
+					case ButtonType.song:
+						break;
+					case ButtonType.ambient:
+						break;
+				}
+			}
 			Debug.Log("Looked");
-			GetComponent<Renderer>().material = inactiveMaterial; 
+			GetComponent<Renderer>().material = activeMaterial; 
 			transform.localScale.Set(3.2f, 3.2f, 1);
 
-		} 
-		else if (!isLookedAt) { 
+		} else if(!isLookedAt) { 
 			Debug.Log("Not Looked");
 
-			GetComponent<Renderer>().material = activeMaterial; 
-			delay = Time.time + 2.0f; 
+			GetComponent<Renderer>().material = inactiveMaterial; 
 			transform.localScale.Set(3, 3, 1); 
 		}
 	}
--- Assets/Scripts/UIScript.cs
+++ Assets/Scripts/UIScript.cs
@@ -8,7 +8,6 @@ public class UIScript : MonoBehaviour {
 	private bool showingMenu;
 	private CardboardHead head;
 
-
 	void Start () {
 		head = Camera.main.GetComponent<StereoController>().Head;
 		pauseMenu.SetActive (false);
@@ -19,18 +18,11 @@ public class UIScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (Cardboard.SDK.Triggered) {
-			if(showingMenu){
-				pauseMenu.transform.parent = head.transform;
-				pauseMenu.SetActive(false);
-				showingMenu = false;
-				Debug.Log("Bye Menu");
-			}else{
-				pauseMenu.transform.parent = null;
+				pauseMenu.transform.SetParent(null);
 				pauseMenu.SetActive(true);
 				showingMenu = true;
 				Debug.Log ("Setting active false");
-			}
-
 		}
 	}
 }
+
