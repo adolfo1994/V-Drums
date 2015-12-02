3ae1dc18f674999c2b2b5f8c4273841433209727
Aldo Culquicondor <alculquicondor@gmail.com>
2015-12-02 17:04:29 -0500

    Logo & splash screnn

---
+++ Assets/Scripts/StartSceneScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartSceneScript : MonoBehaviour {
+
+    float timer;
+
+	void Start () {
+        timer = 2;
+	}
+	
+	void Update () {
+        timer -= Time.deltaTime;
+        if (timer <= 0)
+        {
+            Application.LoadLevel(1);
+        }
+	}
+}
