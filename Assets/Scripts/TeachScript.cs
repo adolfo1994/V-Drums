62495ae41a992e031cde3fcbf9c07fd48dc32ed0
Aldo Culquicondor <alculquicondor@gmail.com>
2015-12-07 13:45:03 -0500

    teacher button

---
+++ Assets/Scripts/TeachScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using Vuforia;
+
+public class TeachScript : MonoBehaviour, IVirtualButtonEventHandler {
+
+    private MetronomeScript metronome;
+    private GameObject button;
+    private bool playing;
+
+    void Start () {
+        GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+        metronome = FindObjectOfType<MetronomeScript>();
+        button = transform.FindChild("Button").gameObject;
+        
+        playing = true;
+    }
+
+    void FixedUpdate () {
+    }
+
+    public void OnButtonPressed(VirtualButtonAbstractBehaviour vb)
+    {
+        button.SetActive(false);
+        playing = !playing;
+        button.GetComponent<Renderer>().material.SetColor("_Color",
+            playing ? new Color(.23f, .7f, .11f) : new Color(.5f, .7f, .5f));
+    }
+
+    public void OnButtonReleased(VirtualButtonAbstractBehaviour vb)
+    {
+        button.SetActive(true);
+    }
+
+}
